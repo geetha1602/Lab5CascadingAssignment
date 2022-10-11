eab961be2c234adc81b810272b011d0bfaf61c1c
geetha1602 <geethapurushotam.b@gmail.com>
2022-10-11 10:04:18 +0530

    Test

---
+++ CascadeDrodown/Controllers/CascadeDDController.cs
@@ -0,0 +1,36 @@
+﻿using Lab5CascadingDropDownApp;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CascadeDrodown.Controllers
+{
+    public class CascadeDDController : Controller
+    {
+        AppDbContext _db;
+        public CascadeDDController(AppDbContext db)
+        {
+            this._db = db;
+        }
+        
+        public JsonResult Country()
+        {
+            var co = _db.Countries.ToList();
+            return new JsonResult(co);
+        }
+
+        public JsonResult City(int id)
+        {
+            var ci = _db.Cities.Where(e => e.Country.CountryId == id).ToList();
+            return new JsonResult(ci);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CasCadeDropDown()
+        {
+            return View();
+        }
+    }
+}
---
+++ CascadeDrodown/wwwroot/js/Cascade.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    GetCountry();
+    $('#City').attr('disabled', true);
+    $('#Country').change(function () {
+        $('#City').attr('disabled', false);
+        var id = $(this).val();
+        $('#City').empty();
+        $('#City').append('<Option>--Select City--</Option>');
+        $.ajax({
+            url: '/CascadeDD/City?id='+id,
+            success: function (result) {
+                $.each(result, function (i, data) {
+                    $('#City').append('<Option value=' + data.cityId + '>' + data.cityName + '</Option>')
+                });
+            }
+        })
+    })
+})
+
+function GetCountry() {
+    $.ajax({
+        url: '/CascadeDD/Country',
+        success: function (result) {
+            $.each(result, function (i, data) {
+                $('#Country').append('<Option value=' + data.countryId + '>' + data.countryName + '</Option>')
+            });
+        }
+    })
+}
